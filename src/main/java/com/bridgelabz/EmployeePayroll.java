34bbb8e896bc60aa7696757a12dd0f854aefcfc8
shuchitasingh <singhshuchita5@gmail.com>
2021-05-19 15:50:58 +0530

    Added: database connection

---
+++ src/main/java/com/bridgelabz/EmployeePayroll.java
@@ -0,0 +1,39 @@
+package com.bridgelabz;
+
+import java.sql.*;
+import java.util.Enumeration;
+
+public class EmployeePayroll {
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "admin1234";
+        Connection connection;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find driver in classpath");
+        }
+        listDrivers();
+        try {
+            System.out.println("Connecting to database: " + jdbcURL);
+            connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection successful: " + connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * List the drivers being loaded
+     */
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println("   " + driverClass.getClass().getName());
+        }
+    }
+}
---
+++ src/test/java/com/bridgelabz/EmployeePayrollTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EmployeePayrollTest
+{
+    
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
