2680b957618967d2c58fb1623e945d5721d894c8
shuchitasingh <singhshuchita5@gmail.com>
2021-05-21 20:50:07 +0530

    add payroll details in databse

--- src/main/java/com/bridgelabz/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/EmployeePayrollService.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class EmployeePayrollService {
 
     public enum IOService {
-        CONSOLE_IO, FILE_IO, DB_IO
+        CONSOLE_IO, FILE_IO, DB_IO,REST_IO
     }
 
     public List<EmployeePayrollData> employeePayrollList;
